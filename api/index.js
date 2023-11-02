90ecb89c9903333f70da86ff2d7ea9c47c4caccb
Chibunud <ndubest56@gmail.com>
2023-11-02 12:09:45 +0100

    db connection

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 const express = require(`express`)
 const cors = require(`cors`)
+// const mongoose = require('mongoose')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+// mongoose.connect(
+//   'mongodb+srv://ikendu:11111234Aa@cluster0.wj5o5ps.mongodb.net/?retryWrites=true&w=majority'
+// )
 
 app.get(`/test`, (req, res) => {
   res.json(`express`)
