72f1b5c1d8fb165ecb03fa41974dfbd4b1b36416
Chibunud <ndubest56@gmail.com>
2023-11-04 17:38:00 +0100

    password encryption

--- api/index.js
+++ api/index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const User = require('./models/User')
+const bcrypt = require('bcryptjs')
 
 const app = express()
 
+const salt = bcrypt.genSaltSync(10)
 app.use(cors())
 app.use(express.json())
 
@@ -16,9 +18,14 @@ app.post('/register', async (req, res) => {
   const { name, email, password } = req.body
 
   try {
-    const userDoc = await User.create({ name, email, password })
+    const userDoc = await User.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, salt),
+    })
     res.json(userDoc)
   } catch (e) {
+    console.log(e)
     res.status(400).json(e)
   }
 
