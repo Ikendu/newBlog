b9cb7ab7567c17d302e556cda89551f29e2e9502
Chibunud <ndubest56@gmail.com>
2023-11-04 16:05:56 +0100

    conecting to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,23 +1,28 @@
-const express = require(`express`)
-const cors = require(`cors`)
-// const mongoose = require('mongoose')
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const User = require('./models/User')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-// mongoose.connect(
-//   'mongodb+srv://ikendu:11111234Aa@cluster0.wj5o5ps.mongodb.net/?retryWrites=true&w=majority'
-// )
+mongoose.connect(
+  'mongodb+srv://blog:33AvNrvMD2pajN2l@cluster0.8fgl6wb.mongodb.net/?retryWrites=true&w=majority'
+)
 
-app.get(`/test`, (req, res) => {
-  res.json(`express`)
-})
-
-app.post(`/register`, (req, res) => {
+app.post('/register', async (req, res) => {
   const { name, email, password } = req.body
-  res.json({ requestData: { name, email, password } })
+
+  try {
+    const userDoc = await User.create({ name, email, password })
+    res.json(userDoc)
+  } catch (e) {
+    res.status(400).json(e)
+  }
+
+  //res.json({ requestData: { name, email, password } })
 })
 
 app.listen(4000)
