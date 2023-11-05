45e684f6786f3f2972d0a992bf4549c6fda15357
Chibunud <ndubest56@gmail.com>
2023-11-05 07:06:55 +0100

    login identification

--- client/src/Contents/Login.jsx
+++ client/src/Contents/Login.jsx
@@ -1,10 +1,33 @@
+import { useState } from 'react'
+
 const Login = () => {
+  const [email, setEmail] = useState(``)
+  const [password, setPassword] = useState(``)
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    await fetch('http://localhost:4000/login', {
+      method: `Post`,
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    })
+  }
   return (
     <>
-      <form className='login'>
+      <form className='login' onSubmit={handleSubmit}>
         <h2>Login Page</h2>
-        <input type='text' placeholder='user name' />
-        <input type='password' placeholder='password' />
+        <input
+          type='text'
+          placeholder='user name'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type='password'
+          placeholder='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button className='btn'>Login here</button>
       </form>
     </>
