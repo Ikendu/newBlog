0d6a0a82993cdad122a5e76e92dc939233c3d54d
Chibunud <ndubest56@gmail.com>
2023-11-05 15:36:32 +0100

    create post page

---
+++ client/src/Contents/CreatePost.jsx
@@ -0,0 +1,11 @@
+const CreatePost = () => {
+  return (
+    <form>
+      <input type='text' placeholder='Title' />
+      <input type='text' placeholder='Summary' />
+      <input type='file' />
+      <button>Create new Post</button>
+    </form>
+  )
+}
+export default CreatePost
