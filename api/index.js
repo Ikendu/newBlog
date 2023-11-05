ff10d0ac9a115026469556d103dbb67c80bac520
Chibunud <ndubest56@gmail.com>
2023-11-05 15:32:59 +0100

    logout functionalities done

--- api/index.js
+++ api/index.js
@@ -4,14 +4,16 @@ const mongoose = require('mongoose')
 const User = require('./models/User')
 const bcrypt = require('bcryptjs')
 const jwt = require(`jsonwebtoken`)
+const cookieParser = require(`cookie-parser`)
 
 const app = express()
 
 const salt = bcrypt.genSaltSync(10)
 const secret = `jbghfrheirufbcvfsjh47jh33hj4h32h25hjhh47hberbb47pajcncvbxnmxn`
 
-app.use(cors())
+app.use(cors({ credentials: true, origin: `http://localhost:5173` }))
 app.use(express.json())
+app.use(cookieParser())
 
 mongoose.connect(
   'mongodb+srv://blog:33AvNrvMD2pajN2l@cluster0.8fgl6wb.mongodb.net/?retryWrites=true&w=majority'
@@ -41,10 +43,10 @@ app.post('/login', async (req, res) => {
   const passOk = bcrypt.compareSync(password, userDoc.password)
   if (passOk) {
     //logged in
-    //res.status(200).json(`logged in`)
+    //res.status(200).json(userDoc._id)
     jwt.sign({ email, id: userDoc._id }, secret, {}, (err, token) => {
       if (err) throw err
-      res.cookie(`token`, token).json(`ok`)
+      res.cookie(`token`, token).json({ email, id: userDoc._id })
     })
   } else {
     //not logged in
@@ -52,4 +54,16 @@ app.post('/login', async (req, res) => {
   }
 })
 
+app.get(`/profile`, (req, res) => {
+  const { token } = req.cookies
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err
+    res.json(info)
+  })
+  res.json(req.cookies)
+})
+
+app.post(`/logout`, (req, res) => {
+  res.cookie(`token`, ``).json(`ok`)
+})
 app.listen(4000)
