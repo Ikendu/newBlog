b0db9456220c5d54c4a1732e3f358013666fc34a
Chibunud <ndubest56@gmail.com>
2023-11-06 15:16:43 +0100

    selecting single post functions

--- client/src/Contents/PostPage.jsx
+++ client/src/Contents/PostPage.jsx
@@ -1,4 +1,29 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
 const PostPage = () => {
-  return <div>PostPage here</div>
+  const [postInfo, setPostInfo] = useState(null)
+
+  const { id } = useParams()
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/post/${id}`).then((resp) => {
+      resp.json().then((userDoc) => {
+        setPostInfo(userDoc)
+      })
+    })
+  }, [])
+
+  if (!postInfo) return ``
+
+  return (
+    <div className='post-page'>
+      <div className='img'>
+        <img src={`http://localhost:4000/${postInfo.cover}`} />
+      </div>
+      <h1>{postInfo.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: postInfo.content }} />
+    </div>
+  )
 }
 export default PostPage
