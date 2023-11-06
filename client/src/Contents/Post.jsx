81234cb5750670a196e93103bd62ea1c7a66ea34
Chibunud <ndubest56@gmail.com>
2023-11-06 12:08:13 +0100

    more create post funtionalities

--- client/src/Contents/Post.jsx
+++ client/src/Contents/Post.jsx
@@ -1,29 +1,36 @@
-import TimeAgo from 'javascript-time-ago'
+//import { formatISO9075 } from 'date-fns'
 
+import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en.json'
 import ru from 'javascript-time-ago/locale/ru.json'
+import { Link } from 'react-router-dom'
 
 import ReactTimeAgo from 'react-time-ago'
 
 TimeAgo.addDefaultLocale(en)
 TimeAgo.addLocale(ru)
 
-const Post = ({ title, summary, content, cover, createdAt }) => {
+const Post = ({ _id, title, summary, content, cover, createdAt, author }) => {
   return (
     <div className='post'>
       <div className='image'>
-        <img src={cover} />
+        <Link to={`/post/${_id}`}>
+          <img src={'http://localhost:4000/' + cover} />
+        </Link>
       </div>
+
       <div className='texts'>
-        <h2>{title}</h2>
+        <Link to={`/post/${_id}`}>
+          <h2>{title}</h2>
+        </Link>
         <p className='info'>
-          <a href='' className='author'>
-            Younglife Ike
-          </a>
+          <a className='author'>{author?.name}</a>
           {/* <time>{formatISO9075(new Date(createdAt))}</time> */}
           <ReactTimeAgo date={createdAt} locale='en-US' />
         </p>
-        <p className='summary'>{summary}</p>
+        <Link to={`/post/${_id}`}>
+          <p className='summary'>{summary}</p>
+        </Link>
       </div>
     </div>
   )
