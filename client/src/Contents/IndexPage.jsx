09dd50f92f6c6c616ca1b5b1575f6bc968c13f35
Chibunud <ndubest56@gmail.com>
2023-11-06 01:52:15 +0100

    retieving post from db

--- client/src/Contents/IndexPage.jsx
+++ client/src/Contents/IndexPage.jsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react'
 import Post from './Post'
 
 const IndexPage = () => {
+  useEffect(() => {
+    fetch(`/post`).then((resp) => {
+      resp.json().then((posts) => {
+        console.log(posts)
+      })
+    })
+  }, [])
   return (
     <div>
       <Post />
