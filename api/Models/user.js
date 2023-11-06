246f69bf20c340ab5fcb4f4f83f9d87f0e45268f
Chibunud <ndubest56@gmail.com>
2023-11-06 12:49:54 +0100

    backend Post control

--- api/Models/user.js
+++ api/Models/user.js
@@ -1,12 +1,15 @@
-const mongoose = require(`mongoose`)
+const mongoose = require('mongoose')
 const { Schema, model } = mongoose
 
 const UserSchema = new Schema({
   name: { type: String, required: true, min: 4 },
-  email: { type: String, required: true, min: 5, unique: true },
+  email: { type: String, required: true, min: 5, uniq: true },
   password: { type: String, required: true },
 })
 
-const UserModel = model(`User`, UserSchema)
+const UserModel = model('User', UserSchema)
 
 module.exports = UserModel
+
+//33AvNrvMD2pajN2l
+//mongodb+srv://blog:33AvNrvMD2pajN2l@cluster0.8fgl6wb.mongodb.net/?retryWrites=true&w=majority
