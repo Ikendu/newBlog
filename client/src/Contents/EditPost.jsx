342849005bd218d779d60d445967e36adba8c015
Chibunud <ndubest56@gmail.com>
2023-11-07 09:20:08 +0100

    more on edit field

--- client/src/Contents/EditPost.jsx
+++ client/src/Contents/EditPost.jsx
@@ -52,18 +52,21 @@ const EditPost = () => {
     data.set(`title`, title)
     data.set(`summary`, summary)
     data.set(`content`, content)
-    data.set(`file`, files?.[0])
+    data.set(`id`, id)
+    if (files?.[0]) {
+      data.set(`file`, files?.[0])
+    }
 
-    console.log(files)
-    const resp = await fetch(`http://localhost:4000/edit`, {
+    //console.log(files)
+    const resp = await fetch(`http://localhost:4000/post`, {
       method: `PUT`,
-      credentials: `include`,
       body: data,
+      credentials: `include`,
     })
 
-    // if (resp.ok) {
-    //   setRedirect(true)
-    // }
+    if (resp.ok) {
+      setRedirect(true)
+    }
   }
   if (redirect) return <Navigate to={'/post/' + id} />
 
