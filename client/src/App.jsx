5fff0b5524b60c463cbd0f8947dd3673bbb07b64
Chibunud <ndubest56@gmail.com>
2023-11-07 05:33:41 +0100

    edith post router doc

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,16 +4,25 @@ import Login from './Contents/Login'
 import { Route, Routes } from 'react-router-dom'
 import SignUp from './Contents/SignUp'
 import IndexPage from './Contents/IndexPage'
+import UserContextProvider from './userContext'
+import CreatePost from './Contents/CreatePost'
+import PostPage from './Contents/PostPage'
+import EditPost from './Contents/EditPost'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<IndexPage />} />
-        <Route path={`/login`} element={<Login />} />
-        <Route path={'/register'} element={<SignUp />} />
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path={`/login`} element={<Login />} />
+          <Route path={'/register'} element={<SignUp />} />
+          <Route path='/create' element={<CreatePost />} />
+          <Route path='/post/:id' element={<PostPage />} />
+          <Route path='/edit/:id' element={<EditPost />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   )
 }
 
